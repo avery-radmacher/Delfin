337fa77f92f602aa9e1f0a8f177f68356927f8fb
Avery Radmacher <45777186+avery-radmacher@users.noreply.github.com>
2021-03-27 10:22:33 -0400

    Decouple UI, IO, and Core (#10)
    * Add decoupled cryption functions
    
    * Add IOHandler and LoadImage
    
    * Add SaveFile and OnSaveFileSuccess
    
    * Add LoadFile and SaveImage
    
    * Include filesize
    
    * Make save file names strings
    
    * Make IOHandler methods take delegates as arguments
    
    * Use IOHandler callbacks inside cryptions
    
    * Use Cryptor for cryptions
    
    * Warn user about cancelling
    
    * Reorder functions
    
    * Remove unused using
    
    * Move IOHandler to separate file
    
    * Move IOHandler to separate project
    
    * Create DelfinCore project
    
    * Remove try-catch on thread arg casts
    
    * Add IBitmapLoader and FileSystemBitmapLoader
    
    * Add default error handler
    
    * Add IWithErrorHandler
    
    * Change to generic ILoader<T>
    
    * Add IHandler<T>
    
    * Add FileSystemByteArrayLoader
    
    * Add FileSystemBitmapHandler
    
    * Add FileSystemByteArrayHandler
    
    * Fix missing set and accesibility modifier
    
    * Remove comment
    
    * Add DecryptorIO
    
    * Add EncryptorIO
    
    * Add IO version of Decrypt
    
    * Rename IO handlers
    
    * Decrypt wraps IO injected method
    
    * Encrypt wraps injected method
    
    * Cancel resets buttons again
    
    * Remove comments and unused usings
    
    * Remove unused class and delegates
    
    * Change location of DefaultErrorHandler
    
    * Supply default parameter
    
    * Move interfaces to separate file
    
    * Rename file
    
    * Remove unused variables
    
    * Remove comment
    
    * Don't join thread
    
    * Remove unnecessary Close and Flush

---
+++ DelfinCore/Cryptor.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Drawing;
+using IOHandler;
+
+namespace DelfinForWindows
+{
+    public class CryptionResult
+    {
+        public bool Success { get; internal set; }
+        public string ErrMsg { get; internal set; }
+        public string ErrDescription { get; internal set; }
+    }
+
+    public class DecryptorIO
+    {
+        internal ILoader<Bitmap> ImageLoader { get; }
+
+        internal IHandler<byte[]> FileSaver { get; }
+
+        public DecryptorIO(ILoader<Bitmap> imageLoader, IHandler<byte[]> fileSaver)
+        {
+            ImageLoader = imageLoader;
+            FileSaver = fileSaver;
+        }
+    }
+
+    public class EncryptorIO
+    {
+        internal ILoader<Bitmap> ImageLoader { get; }
+
+        internal ILoader<byte[]> FileLoader { get; }
+
+        internal IHandler<Bitmap> ImageSaver { get; }
+
+        public EncryptorIO(ILoader<Bitmap> imageLoader, ILoader<byte[]> fileLoader, IHandler<Bitmap> imageSaver)
+        {
+            ImageLoader = imageLoader;
+            FileLoader = fileLoader;
+            ImageSaver = imageSaver;
+        }
+    }
+
+    public class Cryptor
+    {
+        public static void Decrypt(string imgName, string password, string saveFilename, Action<CryptionResult> ProcessResult)
+        {
+            void HandleError(string err, string errDesc)
+            {
+                ProcessResult(new CryptionResult() { Success = false, ErrMsg = err, ErrDescription = errDesc });
+            }
+
+            var imageLoader = new FileSystemBitmapLoader(HandleError) { Filename = imgName };
+            var fileSaver = new FileSystemByteArrayHandler(".zip", HandleError) { Filename = saveFilename };
+            var decryptorIO = new DecryptorIO(imageLoader, fileSaver);
+
+            Decrypt(decryptorIO, password, ProcessResult);
+        }
+
+        public static void Decrypt(DecryptorIO io, string password, Action<CryptionResult> ProcessResult)
+        {
+            void HandleError(string err, string errDesc)
+            {
+                ProcessResult(new CryptionResult() { Success = false, ErrMsg = err, ErrDescription = errDesc });
+            }
+
+            long pixScan = 0, byteScan = -1;
+            int color;
+            byte[] pairBuffer = new byte[6];
+            int population = 0;
+            int datum;
+            Header header = new();
+            byte[] fileBuffer = null;
+            Bitmap img;
+            Cipher cipher = password.Equals("") ? null : new OldCipher(password);
+
+            img = io.ImageLoader.Load();
+            if (img is null) return;
+
+            // main data-processing loop
+            while (!header.IsComplete || byteScan < header.FileSize)
+            {
+                // read a pixel's worth of data from the image
+                color = img.GetPixel((int)(pixScan % img.Width), (int)(pixScan / img.Width)).ToArgb();
+                pairBuffer[population++] = (byte)(color >> 16 & 3);
+                pairBuffer[population++] = (byte)(color >> 8 & 3);
+                pairBuffer[population++] = (byte)(color & 3);
+                pixScan++;
+
+                // write a byte, if we have enough data in the buffer
+                if (population >= 4)
+                {
+                    // retrieve byte from buffer and shift values
+                    datum = (pairBuffer[0] << 6) | (pairBuffer[1] << 4) | (pairBuffer[2] << 2) | pairBuffer[3];
+                    pairBuffer[0] = pairBuffer[4];
+                    pairBuffer[1] = pairBuffer[5];
+                    population -= 4;
+
+                    // determine whether byte is part of header or part of file
+                    if (byteScan < 0)
+                    {
+                        // construct header
+                        if (cipher != null)
+                        {
+                            datum ^= cipher.GetByte();
+                        }
+                        header.AddByte((byte)datum);
+
+                        if (header.IsUnsupported)
+                        {
+                            HandleError("header could not be read or password is wrong", "header could not be read or password is wrong");
+                            return;
+                        }
+
+                        // if header construction is complete, verify it and allocate buffer
+                        if (header.IsComplete)
+                        {
+                            // ensure file fits per completed header
+                            if (img.Height * img.Width * 3 / 4 < header.FileSize + header.HeaderSize)
+                            {
+                                HandleError("file is corrupt or password is wrong", "file is corrupt or password is wrong");
+                                return;
+                            }
+                            else
+                            {
+                                // It seems that occasionally the FileSize can overflow (I believe on invalid password
+                                // that slyly goes unnoticed) and throw a rare exception here. Just quit if so.
+                                try
+                                {
+                                    fileBuffer = new byte[header.FileSize];
+                                }
+                                catch (OverflowException)
+                                {
+                                    HandleError("file is corrupt or password is wrong", "file is corrupt or password is wrong");
+                                    return;
+                                }
+
+                                byteScan = 0; // lets loop know we are no longer reading the header
+                            }
+
+                            // for HV1, replace OldCipher with Cipher if non-null
+                            if (header.HeaderVersion == 1)
+                            {
+                                cipher = cipher == null ? null : new Cipher(password);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // add datum to buffer
+                        fileBuffer[byteScan++] = (byte)datum;
+                    }
+                }
+            }
+
+            // Decrypt file using cipher, if there was a password
+            if (cipher != null)
+            {
+                for (int i = 0; i < fileBuffer.Length; i++)
+                {
+                    fileBuffer[i] ^= cipher.GetByte();
+                }
+            }
+
+            io.FileSaver.Handle(fileBuffer);
+            ProcessResult(new() { Success = true });
+        }
+
+        public static void Encrypt(string imgName, string filename, string password, string saveFilename, Action<CryptionResult> ProcessResult)
+        {
+            void HandleError(string err, string errDesc)
+            {
+                ProcessResult(new CryptionResult() { Success = false, ErrMsg = err, ErrDescription = errDesc });
+            }
+
+            var imageLoader = new FileSystemBitmapLoader(HandleError) { Filename = imgName };
+            var fileLoader = new FileSystemByteArrayLoader(".zip", HandleError) { Filename = filename };
+            var imageSaver = new FileSystemBitmapHandler(HandleError) { Filename = saveFilename };
+            var encryptorIO = new EncryptorIO(imageLoader, fileLoader, imageSaver);
+
+            Encrypt(encryptorIO, password, ProcessResult);
+        }
+
+        public static void Encrypt(EncryptorIO io, string password, Action<CryptionResult> ProcessResult)
+        {
+            void HandleError(string err, string errDesc)
+            {
+                ProcessResult(new CryptionResult() { Success = false, ErrMsg = err, ErrDescription = errDesc });
+            }
+
+            long pixScan = 0, byteScan, fileSize;
+            int pixX, pixY;
+            int color, A, R, G, B;
+            byte[] pairBuffer = new byte[6];
+            int population = 0;
+            int datum;
+            Bitmap img;
+            Header header = new();
+            byte[] headerBuffer;
+            byte[] fileBuffer;
+
+            fileBuffer = io.FileLoader.Load();
+            if (fileBuffer is null) return;
+            fileSize = fileBuffer.LongLength;
+
+            img = io.ImageLoader.Load();
+            if (img is null) return;
+
+            // initiailze header and related items
+            header.FileSize = (int)fileSize;
+            headerBuffer = header.ToBuffer();
+            byteScan = 0 - header.HeaderSize;
+
+            // verify image is large enough to hold the file or quit
+            if (img.Height * img.Width * 3 / 4 < fileSize + header.HeaderSize)
+            {
+                HandleError("image too small", "The image is too small to hold the file.");
+                return;
+            }
+
+            // Encrypt file and header using cipher, if there was a password
+            if (!password.Equals(""))
+            {
+                Cipher cipher = new OldCipher(password);
+                for (int i = 0; i < headerBuffer.Length; i++)
+                {
+                    headerBuffer[i] ^= cipher.GetByte();
+                }
+                cipher = new Cipher(password); // HV1 encrypts the header with OldCipher and the file with Cipher (this ensures backwards compatibility)
+                for (int i = 0; i < fileBuffer.Length; i++)
+                {
+                    fileBuffer[i] ^= cipher.GetByte();
+                }
+            }
+
+            // main data-processing loop
+            while (byteScan < fileSize || population != 0)
+            {
+                // make sure we have data to write; if not, get some more
+                if (population < 3)
+                {
+                    // if there's more data to get, get it; if not, create fake data
+                    if (byteScan < fileSize)
+                    {
+                        if (byteScan < 0)
+                        {
+                            // read a byte from the header
+                            datum = headerBuffer[byteScan + header.HeaderSize];
+                        }
+                        else
+                        {
+                            // read a byte from the file
+                            datum = fileBuffer[byteScan];
+                        }
+
+                        byteScan++;
+
+                        // break it apart and put it in the buffer
+                        for (int i = 3; i >= 0; i--)
+                        {
+                            pairBuffer[population++] = (byte)((datum >> (2 * i)) & 3);
+                        }
+                    }
+                    else
+                    {
+                        // TODO: currently fake data is zeros, can replace later to be original pixel values
+                        while (population < 3)
+                        {
+                            pairBuffer[population++] = 0;
+                        }
+                    }
+                }
+
+                // write a pixel's worth of data to the image
+                pixX = (int)(pixScan % img.Width);
+                pixY = (int)(pixScan / img.Width);
+                color = img.GetPixel(pixX, pixY).ToArgb();
+                A = color >> 24 & 255;
+                R = color >> 16 & 252 | pairBuffer[0];
+                G = color >> 8 & 252 | pairBuffer[1];
+                B = color & 252 | pairBuffer[2];
+                img.SetPixel(pixX, pixY, Color.FromArgb(A, R, G, B));
+                pixScan++;
+
+                // move the buffer values
+                for (int i = 0; i < 3; i++)
+                {
+                    pairBuffer[i] = pairBuffer[i + 3];
+                }
+
+                population -= 3;
+            }
+
+            io.ImageSaver.Handle(img);
+            ProcessResult(new() { Success = true });
+        }
+    }
+}
---
+++ IOHandler/IOHandlers.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace IOHandler
+{
+    public class FileSystemBitmapLoader : ILoader<Bitmap>
+    {
+        public ErrorHandler HandleError { get; }
+
+        public string Filename { get; set; }
+
+        public FileSystemBitmapLoader(ErrorHandler errorHandler = null)
+        {
+            HandleError = errorHandler ?? IWithErrorHandler.DefaultErrorHandler;
+        }
+
+        public Bitmap Load()
+        {
+            FileStream reader;
+            try
+            {
+                reader = new FileStream(Filename, FileMode.Open);
+            }
+            catch (Exception ex) when
+                (ex is ArgumentException ||
+                ex is ArgumentNullException ||
+                ex is DirectoryNotFoundException ||
+                ex is NotSupportedException ||
+                ex is PathTooLongException)
+            {
+                // path is null, empty, or invalid due to length, drive, or characters
+                HandleError("invalid path name", $"The path\r\n{Filename}\r\nis not a valid path. Please specify a valid path.");
+                return null;
+            }
+            catch (FileNotFoundException)
+            {
+                HandleError("file not found", $"The file\r\n{Filename}\r\nwas not found.");
+                return null;
+            }
+            catch (IOException)
+            {
+                HandleError("unexpected I/O error", "An I/O error occurred while opening the file.");
+                return null;
+            }
+            catch (Exception ex) when (ex is System.Security.SecurityException || ex is UnauthorizedAccessException)
+            {
+                HandleError("unauthorized access", $"You don't have permission to access the file:\r\n{Filename}");
+                return null;
+            }
+
+            Bitmap img = null;
+            try
+            {
+                img = new Bitmap(reader);
+            }
+            catch (ArgumentException)
+            {
+                HandleError("invalid image file", $"The file\r\n{Filename}\r\ncould not be interpreted as a valid image.");
+            }
+
+            reader.Dispose();
+
+            return img;
+        }
+    }
+
+    public class FileSystemByteArrayLoader : ILoader<byte[]>
+    {
+        public ErrorHandler HandleError { get; }
+
+        public string Filename { get; set; }
+
+        public string FileExtension { get; }
+
+        public FileSystemByteArrayLoader(string fileExtension, ErrorHandler errorHandler = null)
+        {
+            HandleError = errorHandler ?? IWithErrorHandler.DefaultErrorHandler;
+            FileExtension = fileExtension;
+        }
+
+        public byte[] Load()
+        {
+            if (!Filename.EndsWith(FileExtension))
+            {
+                HandleError("Wrong file type", $"Expected {Filename} to end with {FileExtension}");
+                return null;
+            }
+
+            long fileSize;
+            byte[] fileBuffer;
+            try
+            {
+                fileSize = new FileInfo(Filename).Length;
+                fileBuffer = File.ReadAllBytes(Filename);
+            }
+            catch (Exception ex) when
+                (ex is ArgumentException ||
+                ex is ArgumentNullException ||
+                ex is PathTooLongException ||
+                ex is DirectoryNotFoundException ||
+                ex is NotSupportedException)
+            {
+                // path is null, empty, or invalid due to length, drive, or characters
+                HandleError("invalid path name", $"The path\r\n{Filename}\r\nis not a valid path. Please specify a valid path.");
+                return null;
+            }
+            catch (FileNotFoundException)
+            {
+                HandleError("file not found", $"The file\r\n{Filename}\r\nwas not found.");
+                return null;
+            }
+            catch (IOException)
+            {
+                HandleError("unexpected I/O error", "An I/O error occurred while opening the file.");
+                return null;
+            }
+            catch (Exception ex) when (ex is UnauthorizedAccessException || ex is System.Security.SecurityException)
+            {
+                HandleError("unauthorized access", $"You don't have permission to access the file:\r\n{Filename}");
+                return null;
+            }
+
+            if (fileBuffer.LongLength != fileSize) throw new ApplicationException("Buffers can be other sizes, apparently.");
+            return fileBuffer;
+        }
+    }
+
+    public class FileSystemBitmapHandler : IHandler<Bitmap>
+    {
+        public ErrorHandler HandleError { get; }
+
+        public string Filename { get; set; }
+
+        public FileSystemBitmapHandler(ErrorHandler errorHandler = null)
+        {
+            HandleError = errorHandler ?? IWithErrorHandler.DefaultErrorHandler;
+        }
+
+        public void Handle(Bitmap item)
+        {
+            if (!Filename.EndsWith(".png"))
+            {
+                HandleError("Wrong file type", $"Expected {Filename} to be a .png file.");
+                return;
+            }
+
+            FileStream writer;
+            try
+            {
+                writer = new FileStream(Filename, FileMode.Create);
+            }
+            catch (Exception ex) when
+                (ex is ArgumentException ||
+                ex is NotSupportedException ||
+                ex is ArgumentNullException ||
+                ex is DirectoryNotFoundException ||
+                ex is PathTooLongException)
+            {
+                // path is null, empty, or invalid due to length, drive, or characters
+                HandleError("invalid path name", $"The path\r\n{Filename}\r\nis not a valid path. Please specify a valid path.");
+                return;
+            }
+            catch (IOException)
+            {
+                HandleError("unexpected I/O error", "An I/O error occurred while using the file.");
+                return;
+            }
+            catch (System.Security.SecurityException)
+            {
+                HandleError("unauthorized access", $"You don't have permission to access the file:\r\n{Filename}");
+                return;
+            }
+
+            item.Save(writer, System.Drawing.Imaging.ImageFormat.Png);
+            writer.Dispose();
+        }
+    }
+
+    public class FileSystemByteArrayHandler : IHandler<byte[]>
+    {
+        public ErrorHandler HandleError { get; }
+
+        public string Filename { get; set; }
+
+        public string FileExtension { get; }
+
+        public FileSystemByteArrayHandler(string fileExtension, ErrorHandler errorHandler = null)
+        {
+            HandleError = errorHandler ?? IWithErrorHandler.DefaultErrorHandler;
+            FileExtension = fileExtension;
+        }
+
+        public void Handle(byte[] item)
+        {
+            if (!Filename.EndsWith(FileExtension))
+            {
+                HandleError("Wrong file type", $"Expected {Filename} to end with {FileExtension}");
+                return;
+            }
+
+            BinaryWriter writer;
+            try
+            {
+                writer = new BinaryWriter(File.Open(Filename, FileMode.Create));
+                writer.Write(item);
+            }
+            catch (Exception ex) when
+                (ex is ArgumentException ||
+                ex is ArgumentNullException ||
+                ex is PathTooLongException ||
+                ex is DirectoryNotFoundException ||
+                ex is NotSupportedException)
+            {
+                // path is null, empty, or invalid due to length, drive, or characters
+                HandleError("invalid path name", $"The path\r\n{Filename}\r\nis not a valid path. Please specify a valid path.");
+                return;
+            }
+            catch (IOException)
+            {
+                HandleError("unexpected I/O error", "An I/O error occurred while using the file.");
+                return;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                HandleError($"You don't have permission to access the file:\r\n{Filename}", "unauthorized access");
+                return;
+            }
+
+            writer.Dispose();
+        }
+    }
+}
